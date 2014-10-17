156d35f1a6242b9009edf23d0d212cbfce851a86
Xiaolei Jin <xiaoleij@andrew.cmu.edu>
2014-10-17 16:29:44 -0700

    Update src

--- src/ImplementStrStr.cpp
+++ src/ImplementStrStr.cpp
@@ -9,3 +9,23 @@ public:
     }
 };
 
+class Solution {
+public:
+    char *strStr(char *haystack, char *needle) {
+        if (haystack && needle) {
+            int len1 = strlen(haystack);
+            int len2 = strlen(needle);
+            for (int i = 0; i < len1 - len2 + 1; ++i) {
+                char *s1 = haystack + i, *s2 = needle;
+                while (*s1 == *s2 && *s2 != '\0') {
+                    s1++, s2++;
+                }
+                if (*s2 == '\0') {
+                    return haystack + i;
+                }
+            }
+        }
+        return NULL;
+    }
+};
+
